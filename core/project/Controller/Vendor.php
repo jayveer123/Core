c3ff683478db516674880b65d1ead7816aedcc44
jayveer123 <jadejajayveersinh1000@gmail.com>
2022-03-01 14:28:03 +0530

    Complete Project with three module

--- core/project/Controller/Vendor.php
+++ core/project/Controller/Vendor.php
@@ -16,15 +16,109 @@ class Controller_Vendor extends Controller_Core_Action{
 
 	public function editAction()
 	{
+		$vendorModel = Ccc::getModel('Vendor');
+		$request = $this->getRequest();
+		$id = (int)$request->getRequest('id');
+		if(!$id)
+		{
+			throw new Exception("Invalid Request", 1);
+		}
 		
+		$vendor=$vendorModel->load($id);
+		
+		if(!$vendor)
+		{	
+			throw new Exception("System is unable to find record.", 1);	
+		}
+		/*$addressModel = Ccc::getModel('Customer_Address');
+		$address = $addressModel->load($id,'customer_id');
+		if(!$address)
+		{
+			$address = ['customer_id' => $customer['customer_id']];	
+		}*/
+		Ccc::getBlock('Vendor_Edit')->addData('vendor',$vendor)->toHtml();
 	}
 	public function deleteAction()
 	{
-		
+		try{
+			$vendorModel = Ccc::getModel('Vendor');
+			$request = $this->getRequest();
+			if(!$request->getRequest('id'))
+			{
+				throw new Exception("Invalid Request.", 1);
+			}
+
+			$vendorId = $request->getRequest('id');
+
+			if(!$vendorId)
+			{
+				throw new Exception("Unable to fetch ID.", 1);
+			}
+			$vendor = $vendorModel;
+
+			$result = $vendor->load($vendorId)->delete();
+			if(!$result)
+			{
+				throw new Exception("Unable to Delet Record.", 1);
+			}
+		    $this->redirect($this->getView()->getUrl('vendor','grid',[],true));
+		}
+		catch(Exception $e){
+			echo "<pre>";
+			print_r($e);
+		}
+	}
+	public function saveVendor()
+	{
+		$vendorModel = Ccc::getModel('Vendor');
+		$request = $this->getRequest();
+		if(!$request->getPost('vendor'))
+		{
+			throw new Exception("Invalid Request", 1);
+		}	
+		$postData = $request->getPost('vendor');
+		if(!$postData)
+		{
+			throw new Exception("Invalid data posted.", 1);	
+		}
+		$vendor = $vendorModel;
+		$vendor->setData($postData);
+
+		if($vendor->id==null)
+		{
+			unset($vendor->id);
+			$vendor->createdDate = date('y-m-d h:m:s');
+			$insert = $vendor->save();
+			if($insert==null)
+			{
+				throw new Exception("System is unable to Insert.", 1);
+			}
+			return $insert;
+		}
+		else
+		{
+			if(!(int)$vendor->id)
+			{
+				throw new Exception("Invalid Request.", 1);
+			}
+			$vendor->updatedDate = date('y-m-d h:i:s');
+			$update = $vendor->save();
+		}	 
 	}
 	public function saveAction()
 	{
-		
+		error_reporting(0);
+
+		try{
+			$vendorId=$this->saveVendor();
+			
+
+			$this->redirect($this->getView()->getUrl('vendor','grid',[],true));
+		}
+		catch(Exception $e){
+			echo "<pre>";
+			print_r($e);
+		}
 	}
 	public function errorAction()
 	{
